11a1d8edf94b7344cbf7691558d01e098d0adcdc
1373387721 <1373387721@qq.com>
2018-10-06 18:26:12 +0800

    客户名字不可重复(唯一性验证)

--- models/res_partner.py
+++ models/res_partner.py
@@ -14,7 +14,26 @@ class res_partner(models.Model):
     is_default = fields.Boolean('是否为默认地址')
     city_domain_ids = fields.One2many('oe.city', compute='_compute_city_domain_ids')
     district_domain_ids = fields.One2many('oe.district', compute='_compute_district_domain_ids')
+    
+    """
+    唯一性验证
+    方法一：利用_sql_contraints
+    注意：如果添加_sql_contraints前已经存在相同数据的字段，则_sql_contraints是添加不上的，因此也不会有提示出现。
+    """
+    _sql_constraints = [(
+        'res_partner_name_unique',
+        'UNIQUE (name)',
+        '名字已存在！'
+    )]
 
+    """
+    方法二：
+    @api.contrains('name')
+    def _check_name(self):
+        partners = self.search([('name', '=', self.name)])
+        if len(partners) > 1:
+            raise ValueError('名字%s已存在!'%self.name)
+    """
 
     @api.onchange('province_id')
     def _onchange_province_id(self):
